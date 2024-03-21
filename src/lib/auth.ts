d12c9cfd3df6941eba7072b87c31f69de5466b22
Ralf Boltshauser <ralf@boltshauser.com>
2024-03-21 02:22:19 +0100

    remove credentials provider

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,5 @@
 import { prisma } from "@/lib/client";
-import bcrypt from "bcryptjs";
 import { NextAuthOptions, getServerSession } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 
 export const config = {
@@ -17,72 +15,72 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
-    CredentialsProvider({
-      // The name to display on the sign in form (e.g. "Sign in with...")
-      name: "Credentials",
-      // `credentials` is used to generate a form on the sign in page.
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
-      credentials: {
-        name: { label: "Name", type: "text" },
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" },
-        confirmPassword: { label: "Confirm Password", type: "password" },
-      },
-      async authorize(user: any, req: any) {
-        if (!user.email) {
-          return false;
-        }
+    // CredentialsProvider({
+    //   // The name to display on the sign in form (e.g. "Sign in with...")
+    //   name: "Credentials",
+    //   // `credentials` is used to generate a form on the sign in page.
+    //   // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+    //   // e.g. domain, username, password, 2FA token, etc.
+    //   // You can pass any HTML attribute to the <input> tag through the object.
+    //   credentials: {
+    //     name: { label: "Name", type: "text" },
+    //     email: { label: "Email", type: "email" },
+    //     password: { label: "Password", type: "password" },
+    //     confirmPassword: { label: "Confirm Password", type: "password" },
+    //   },
+    //   async authorize(user: any, req: any) {
+    //     if (!user.email) {
+    //       return false;
+    //     }
 
-        const dbUser = await prisma.user.findUnique({
-          where: { email: user.email! },
-        });
+    //     const dbUser = await prisma.user.findUnique({
+    //       where: { email: user.email! },
+    //     });
 
-        let userId = dbUser?.id;
+    //     let userId = dbUser?.id;
 
-        if (dbUser && dbUser.password) {
-          // user already exists validate password
-          const match = await bcrypt.compare(user.password, dbUser.password);
-          if (!match) {
-            throw new Error("Wrong password");
-          }
-        } else if (
-          user.name &&
-          user.email &&
-          user.password &&
-          user.confirmPassword
-        ) {
-          const passwordMatches = user.password === user.confirmPassword;
+    //     if (dbUser && dbUser.password) {
+    //       // user already exists validate password
+    //       const match = await bcrypt.compare(user.password, dbUser.password);
+    //       if (!match) {
+    //         throw new Error("Wrong password");
+    //       }
+    //     } else if (
+    //       user.name &&
+    //       user.email &&
+    //       user.password &&
+    //       user.confirmPassword
+    //     ) {
+    //       const passwordMatches = user.password === user.confirmPassword;
 
-          if (!passwordMatches) {
-            throw new Error("Passwords do not match");
-          }
+    //       if (!passwordMatches) {
+    //         throw new Error("Passwords do not match");
+    //       }
 
-          const hash = await bcrypt.hash(user.password, 10);
+    //       const hash = await bcrypt.hash(user.password, 10);
 
-          const userResponse = await prisma.user.upsert({
-            where: { email: user.email! },
-            update: {
-              name: user.name!,
-              email: user.email!,
-            },
-            create: {
-              name: user.name!,
-              email: user.email!,
-              password: hash,
-            },
-          });
-          userId = userResponse.id;
-        } else {
-          throw new Error("Something went wrong!");
-        }
+    //       const userResponse = await prisma.user.upsert({
+    //         where: { email: user.email! },
+    //         update: {
+    //           name: user.name!,
+    //           email: user.email!,
+    //         },
+    //         create: {
+    //           name: user.name!,
+    //           email: user.email!,
+    //           password: hash,
+    //         },
+    //       });
+    //       userId = userResponse.id;
+    //     } else {
+    //       throw new Error("Something went wrong!");
+    //     }
 
-        // TODO if you need to add more properties to the token you can do it here
-        user.dbId = userId;
-        return user;
-      },
-    }),
+    //     // TODO if you need to add more properties to the token you can do it here
+    //     user.dbId = userId;
+    //     return user;
+    //   },
+    // }),
   ],
   pages: {
     signIn: "/auth/sign-in",
