e44c124375861a75d2d63f68ddff9479a0228836
oiver12 <oliver.baumgartner12@gmail.com>
2024-03-22 00:17:03 +0100

    Menu refresh

--- src/app/onboarding/actions.ts
+++ src/app/onboarding/actions.ts
@@ -4,6 +4,7 @@ import { prisma } from "@/lib/client";
 import { getServerSession } from "next-auth";
 import { IInterest } from "./selectPreferences";
 import { redirect } from "next/navigation";
+import { Language } from "@prisma/client";
 
 export async function selectInterests(interests: IInterest[]) {
   const session = await getServerSession(authOptions);
@@ -23,19 +24,21 @@ export async function selectInterests(interests: IInterest[]) {
       },
     },
   });
+}
 
-  // await prisma.user.update({
-  //     where: {
-  //         id: session.user.dbId,
-  //     },
-  //     data: {
-  //         interests: {
-  //             connect: {
-  //                 id:
-  //             }
-  //         },
-  //     },
-  // })
+export async function selectLanguage(language: Language) {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    throw new Error("Unauthorized");
+  }
+  await prisma.user.update({
+    where: {
+      id: session.user.dbId,
+    },
+    data: {
+      language: language,
+    },
+  });
 }
 
 export async function selectFacility(facilityId: number) {
