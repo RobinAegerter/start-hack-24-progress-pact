ac12a8b183f71a2e0cb56a96ae67a97a728456b1
oiver12 <oliver.baumgartner12@gmail.com>
2024-03-21 12:43:08 +0100

    ChatBox CleanUp

---
+++ src/app/coach/Chatbot.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { PaperPlaneIcon } from "@radix-ui/react-icons";
+import { useChat } from "ai/react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import { useState } from "react";
+
+export default function ChatBotComponent({ userImage }: { userImage: string }) {
+  const clickButton = (text: string) => {
+    // Call handleInputChange
+    handleInputChange({
+      target: { value: text },
+    } as any);
+
+    // Use a second setTimeout to ensure the state update from handleInputChange
+    // has taken place, before calling handleSubmit
+    setTimeout(() => {
+      handleSubmit({ preventDefault: () => {} } as any);
+    }, 200);
+  };
+
+  const defaultQuestions = () => {
+    return (
+      <div className="flex flex-col gap-2">
+        <p>Ask a question</p>
+        <Button
+          className="m-2 p-2 whitespace-normal"
+          variant={"outline"}
+          onClick={() => {
+            clickButton(
+              "Can you help me set a realistic exercise goal for the month?"
+            );
+          }}
+        >
+          Can you help me set a realistic exercise goal for the month?
+        </Button>
+        <Button
+          className="m-2 whitespace-normal"
+          variant={"outline"}
+          onClick={() =>
+            clickButton(
+              "How many calories should I aim to eat each day if I want to lose weight?"
+            )
+          }
+        >
+          How many calories should I aim to eat each day if I want to lose
+          weight?
+        </Button>
+        <Button
+          className="m-2 whitespace-normal"
+          variant={"outline"}
+          onClick={() =>
+            clickButton(
+              "What are some key tips for setting realistic health and nutrition goals?"
+            )
+          }
+        >
+          What are some key tips for setting realistic health and nutrition
+          goals?
+        </Button>
+      </div>
+    );
+  };
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  return (
+    <div className="flex flex-col w-full max-w-md mx-auto stretch">
+      <h2 className="text-3xl mb-5">Coach</h2>
+      {messages.map((m) => (
+        <div key={m.id} className="whitespace-pre-wrap">
+          <div className="flex items-center mb-2">
+            <Avatar className="w-9 h-9">
+              <AvatarImage
+                src={
+                  m.role === "user" ? userImage : "/android-chrome-192x192.png"
+                }
+              />
+            </Avatar>
+            <h2 className="font-bold text-base ml-1">
+              {m.role === "user" ? "User" : "AI"}
+            </h2>
+          </div>
+          <p className="m-5">{m.content}</p>
+        </div>
+      ))}
+      {messages.length === 0 ? defaultQuestions() : <div></div>}
+      <form
+        onSubmit={handleSubmit}
+        className="flex gap-2 flex-row fixed bottom-0 w-[calc(100%-1rem)] left-0 p-4 bg-white shadow-md"
+      >
+        <Input
+          className=""
+          value={input}
+          placeholder="Say something..."
+          onChange={handleInputChange}
+        />
+        <Button size="icon">
+          <PaperPlaneIcon />
+        </Button>
+      </form>
+    </div>
+  );
+}
--- src/app/coach/page.tsx
+++ src/app/coach/page.tsx
@@ -1,36 +1,14 @@
-"use client";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import ChatBotComponent from "./Chatbot";
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { PaperPlaneIcon } from "@radix-ui/react-icons";
-import { useChat } from "ai/react";
+export default async function Chat() {
+  const session = await getServerSession(authOptions);
+  if (!session) return null;
+  let userImageUrl = "/default_user.png";
+  if (session.user.image) {
+    userImageUrl = session.user.image;
+  }
 
-export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
-  return (
-    <div className="flex flex-col w-full max-w-md mx-auto stretch">
-      <h2 className="text-2xl">Coach</h2>
-      {messages.map((m) => (
-        <div key={m.id} className="whitespace-pre-wrap">
-          {m.role === "user" ? "User: " : "AI: "}
-          {m.content}
-        </div>
-      ))}
-
-      <form
-        onSubmit={handleSubmit}
-        className="flex gap-2 flex-row fixed bottom-0 w-[calc(100%-1rem)] left-0 p-4 bg-white shadow-md"
-      >
-        <Input
-          className=""
-          value={input}
-          placeholder="Say something..."
-          onChange={handleInputChange}
-        />
-        <Button size="icon">
-          <PaperPlaneIcon />
-        </Button>
-      </form>
-    </div>
-  );
+  return <ChatBotComponent userImage={userImageUrl} />;
 }
