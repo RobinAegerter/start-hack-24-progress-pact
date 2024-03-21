8ed2db11b65787cb679c4bc88451be720db8678e
Ralf Boltshauser <ralf@boltshauser.com>
2024-03-21 02:18:26 +0100

    colors & font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,10 @@ import Navbar from "@/components/Navbar/Navbar";
 import NextAuthProvider from "@/components/auth/NextAuthProvider";
 import { Toaster } from "@/components/ui/sonner";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Poppins({ subsets: ["latin"], weight: "400" });
 
 export const metadata: Metadata = {
   title: "Progress Pact",
@@ -19,6 +19,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff"></meta>
+      </head>
       <body className={inter.className}>
         <NextAuthProvider>
           <Navbar />
