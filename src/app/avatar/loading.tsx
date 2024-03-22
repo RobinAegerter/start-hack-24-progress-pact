e4e931e1bbe4ea95a83b6bce72a8a5bde6ee122b
RobinAegerter <robin.aegi@bluewin.ch>
2024-03-22 06:23:22 +0100

    Fixes loading indicator and model animation from saemi

--- src/app/avatar/loading.tsx
+++ src/app/avatar/loading.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
+
 export default function Loading() {
   return (
     <div className="flex justify-center items-center h-screen w-screen">
-      <img src={"/model_loading.gif"} alt="loading" />
+      <Image
+        src={"/model_loading.gif"}
+        width={"400"}
+        unoptimized
+        height={400}
+        alt="loading"
+      />
     </div>
   );
 }
